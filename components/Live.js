e83375d5b14fdc27385f28ed0f0dc6522d9456d6
Kenya Sullivan <kenya@stackpoets.com>
2017-11-15 11:27:00 -0500

    Live speed and altitude tracking

--- components/Live.js
+++ components/Live.js
@@ -8,14 +8,64 @@ import {
 } from "react-native";
 import { Foundation } from "@expo/vector-icons";
 import { purple, white } from "../utils/colors";
+import { Location, Permissions } from "expo";
+import { calculateDirection } from "../utils/helpers";
 
 export default class Live extends Component {
   state = {
-    coords: null,
+    coords: {
+      altitude: 1,
+      speed: 1
+    },
     status: "granted",
     direction: ""
   };
-  askPermission = () => {};
+
+  componentDidMount() {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+
+        this.setState(() => ({ status }));
+      })
+      .catch(error => {
+        console.warn("Error getting Location permission: ", error);
+
+        this.setState(() => ({ status: "undetermined" }));
+      });
+  }
+  askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(() => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+        this.setState(() => ({ status }));
+      })
+      .catch(error => console.warn("error asking Location permissions", error));
+  };
+
+  setLocation = () => {
+    Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        timeInterval: 1,
+        distanceInterval: 1
+      },
+      ({ coords }) => {
+        const newDirection = calculateDirection(coords.heading);
+        const { direction, bounceValue } = this.state;
+
+        this.setState(() => ({
+          coords,
+          status: "granted",
+          direction: newDirection
+        }));
+      }
+    );
+  };
   render() {
     const { status, coords, direction } = this.state;
 
@@ -51,16 +101,20 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>North</Text>
+          <Text style={styles.direction}>{direction}</Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Altitude</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {Math.round(coords.altitude * 3.2808)} Feet
+            </Text>
           </View>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Speed</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
           </View>
         </View>
       </View>
